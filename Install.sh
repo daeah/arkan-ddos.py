905f21208d412a38a114079f1c66e14b913c36af
daeah <83104993+daeah@users.noreply.github.com>
2021-04-24 13:43:38 +0700

    Install. sh

--- Install.sh
+++ Install.sh
@@ -2,7 +2,7 @@ clear
 echo """
 		 ____________________________________________
 		|					                         |
-		|          The Code By :- Mohamed HaCker     |
+		|          The Code By :-arkan y.                |
 		|	        whatsapp: 085855744883      |
 		|           follow ig : arkan_y 
 		|          facebook :- https://yetteno.yetteno.7161  |
@@ -10,5 +10,5 @@ echo """
 		|          Git Hub :- github.com/Daeah arkan     |
 		|____________________________________________| """
 pkg install figlet -y 
-cp -a DDos-Anonymous.py /data/data/com.termux/files/usr/bin
-echo "run [DDos-Anonymous.py] in now tabe "
+cp -a arkan-DDOS.py /data/data/com.termux/files/usr/bin
+echo "run [arkan-DDOS.py ] in now tabe "
